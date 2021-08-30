8a292a64d41fdc40a16485e6adc2292fa8ebdf1a
ntavora <ntavora@devsutd.com>
2021-08-30 16:33:11 -0300

    App de gastos

---
+++ presupuesto/src/App.js
@@ -0,0 +1,14 @@
+
+import Pregunta from "./components/Pregunta";
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Gasto Semanal</h1>
+      </div>
+      <Pregunta/>
+    </div>
+  );
+}
+
+export default App;
